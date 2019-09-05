643eaca6409758702c3e6868569f145e4c446abb
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-09-05 16:44:55 -0300

    changes following apple doc

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,35 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Mateus Rovari on 05/09/19.
+//  Copyright © 2019 Mateus Rovari. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
