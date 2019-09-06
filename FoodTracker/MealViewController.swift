09286c5f3ce91f19b75ee3764b3ce8c54dd52cc7
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-09-06 10:56:42 -0300

    deleting meals

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -43,6 +43,17 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+        
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAndMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAndMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     // This method lets you configure a view controller before it's presented
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
